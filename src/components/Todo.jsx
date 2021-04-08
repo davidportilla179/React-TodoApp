3e4254c380907e378b1f9ca233c0d3c30daeb19a
David Portilla <davidportilla179@hotmail.com>
2021-04-07 23:14:21 -0500

    fixed line-through all tasks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import "../css/Todo.css";
 
 class Todo extends React.Component {
+  state ={
+    done: false
+  }
+
+  handleClick = () => {
+    this.setState({
+      done: !this.state.done
+    })
+    console.log('si jala xD, valor de done: ' + String(this.state.done));
+  }
   render() {
     return(
     <div className="list-item">
-      <input type="checkbox" 
+      <input type="checkbox"
           className="ok"
-          defaultChecked={this.props.done}
-          onChange={this.props.handleClick}
+          defaultChecked={this.state.done}
+          onChange={this.handleClick}
       />
-      <p className={`${this.props.done ? 'done' : ''}`}>Tarea</p>
+      <p className={`${this.state.done ? 'done' : ''}`}>{this.props.title}</p>
       <div className="buttons">
         <button className="delete">
           {'\u292b'}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,31 +3,24 @@ import Todo from './Todo';
 import "../css/TodoList.css";
 
 class TodoList extends React.Component {
-  state ={
-    done: false
+  state = {
+    tasks: [
+      {title: 'Tarea 1'},
+      {title: 'Tarea 2'},
+      {title: 'Tarea 3'},
+      {title: 'Tarea 4'},
+    ]
   }
 
-  handleClick = () => {
-    this.setState({
-      done: !this.state.done
-    })
-    console.log('si jala xD, valor de done: ' + String(this.state.done));
-  }
   render() {
     return (
       <div className="list-wrapper">
-        <Todo 
-          done={this.state.done}
-          handleClick={this.handleClick}/>
-        <Todo 
-          done={this.state.done}
-          handleClick={this.handleClick}/>
-        <Todo 
-          done={this.state.done}
-          handleClick={this.handleClick}/>
-        <Todo
-          done={this.state.done}
-          handleClick={this.handleClick}/>
+        {
+          this.state.tasks.map((task,index) => {
+            return <Todo title={task.title} key={index} />
+          })
+        }
+
       </div>
     )
   }
